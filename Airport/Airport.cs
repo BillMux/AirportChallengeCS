09377e9b08666c2acfef6ff1df42d11b02acef95
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-09-09 23:08:05 +0100

    Fix name change break?

---
+++ Airport/Airport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace AirportClass
+{
+    public class Airport
+    {
+        static void Main()
+        {
+
+        }
+
+        public ArrayList hangar = new ArrayList();
+
+        public ArrayList Land(string plane)
+        {
+            hangar.Add(plane);
+            return hangar;
+        }
+    }
+}
---
+++ AirportChallengeCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirportChallengeCS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
